071fa62833161dce6e0fa67979221fc120ee423e
Sergei Serdiuk <sserdiuk@luxoft.com>
2021-05-13 11:32:38 +0300

    Refactored code.

---
+++ XO/MoveDatasource.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace XO
+{
+    internal class MoveDatasource
+    {
+        private ISet<int> _previousMoves = new HashSet<int>();
+
+        public MoveDatasource()
+        {
+        }
+
+        public int GetNextMove(string playerName)
+        {
+            string raw_cell;
+            int cell;
+
+            do
+            {
+                Console.Write($"{playerName},введите номер ячейки,сделайте свой ход:");
+
+                raw_cell = Console.ReadLine();
+            }
+            while (!Int32.TryParse(raw_cell, out cell));
+
+            while (cell > 9 || cell < 1 || _previousMoves.Contains(cell))
+            {
+                do
+                {
+                    Console.Write("Введите номер правильного ( 1-9 ) или пустой ( --- ) клетки , чтобы сделать ход:");
+                    raw_cell = Console.ReadLine();
+                }
+                while (!Int32.TryParse(raw_cell, out cell));
+
+                Console.WriteLine();
+            }
+
+            return cell;
+        }
+    }
+}
---
+++ XO/Player.cs
@@ -0,0 +1,14 @@
+﻿namespace XO
+{
+    internal class Player
+    {
+        public string Name { get; }
+        public char Marker { get; }
+
+        public Player(string name, char marker)
+        {
+            Name = name;
+            Marker = marker;
+        }
+    }
+}
